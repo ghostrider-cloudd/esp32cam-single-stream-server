eb094880aa82252de4920d92778a849b9b965d36
Arjun M <arjunmehandrakar06@gmail.com>
2025-11-16 11:45:29 +0530

    Add files via upload

---
+++ ESP32_CAM_Video_Stream.ino
@@ -0,0 +1,55 @@
+#include "esp_camera.h"
+#include <WiFi.h>
+
+const char* ssid = "realme85G";
+const char* password = "11111111";
+
+camera_config_t config = {
+  .pin_pwdn = 32,
+  .pin_reset = -1,
+  .pin_xclk = 0,
+  .pin_sccb_sda = 26,
+  .pin_sccb_scl = 27,
+  .pin_d7 = 35,
+  .pin_d6 = 34,
+  .pin_d5 = 39,
+  .pin_d4 = 36,
+  .pin_d3 = 21,
+  .pin_d2 = 19,
+  .pin_d1 = 18,
+  .pin_d0 = 5,
+  .pin_vsync = 25,
+  .pin_href = 23,
+  .pin_pclk = 22,
+  .xclk_freq_hz = 20000000,
+  .ledc_timer = LEDC_TIMER_0,
+  .ledc_channel = LEDC_CHANNEL_0,
+  .pixel_format = PIXFORMAT_JPEG,
+  .frame_size = FRAMESIZE_QVGA,
+  .jpeg_quality = 12,
+  .fb_count = 1
+};
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) delay(500);
+
+  if (esp_camera_init(&config) != ESP_OK) {
+    Serial.println("Camera init failed");
+    return;
+  }
+  Serial.println("Camera init success");
+}
+
+void loop() {
+  // Just test capture
+  camera_fb_t * fb = esp_camera_fb_get();
+  if (fb) {
+    Serial.printf("Captured frame: %u bytes\n", fb->len);
+    esp_camera_fb_return(fb);
+  } else {
+    Serial.println("Capture failed");
+  }
+  delay(1000);
+}
